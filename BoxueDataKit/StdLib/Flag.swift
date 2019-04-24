46828956ef42836ddf78df4f1196decb9101c05d
ArcherLj <archerlj@ArcherLjdeMacBook-Pro.local>
2019-04-24 10:14:31 +0800

    travis.yml文件修改

--- BoxueDataKit/StdLib/Flag.swift
+++ BoxueDataKit/StdLib/Flag.swift
@@ -10,6 +10,50 @@ import Foundation
 import UserNotifications
 
 final public class Flag {
-    public static let isFirstLaunch = true
-    public static let isiOS12OrLater = false
+    
+    #if TEST
+        public static let isFirstLaunch = true
+    #else
+        public static let isFirstLaunch = Flag(for: "io.boxue.Launch.wasLaunchedBefore").wasNotSet
+    #endif
+    
+    #if TEST
+        public static let isiOS12OrLater = false
+    #else
+    public static var isiOS12OrLater: Bool {
+        if #available(iOS 12.0, *) {
+            return true
+        }
+        return false
+    }
+    #endif
+    
+    
+    /// - Typealias
+    public typealias Getter = () -> Bool
+    public typealias Setter = (Bool) -> Void
+    
+    /// - Attributes
+    var wasSet: Bool
+    public var wasNotSet: Bool {
+        return !wasSet
+    }
+    
+    /// - Initializers
+    public init(getter: Getter, setter: Setter) {
+        self.wasSet = getter()
+        
+        if !wasSet {
+            setter(true)
+        }
+    }
+    
+    convenience init(userDefaults: UserDefaults = UserDefaults.standard,
+                     for key: String) {
+        self.init(getter: { () -> Bool in
+            return userDefaults.bool(forKey: key)
+        }) {
+            userDefaults.set($0, forKey: key)
+        }
+    }
 }
